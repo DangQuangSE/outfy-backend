97dc7eb20e5d4b779542d29bbc24d50eb47e3b4a
DangQuangSE <quangndse180246@fpt.edu.vn>
2026-03-16 12:26:54 +0700

    add document

---
+++ src/main/java/com/outfy/outfy_backend/OutfyBackendApplication.java
@@ -0,0 +1,40 @@
+package com.outfy.outfy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.Objects;
+
+@SpringBootApplication
+@PropertySources({
+		@PropertySource("classpath:application.properties"),
+		@PropertySource(value = "file:.env", ignoreResourceNotFound = true)
+})
+public class OutfyBackendApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+		if (dotenv != null) {
+			System.setProperty("DB_HOST", Objects.toString(dotenv.get("DB_HOST")));
+			System.setProperty("DB_PORT", Objects.toString(dotenv.get("DB_PORT")));
+			System.setProperty("DB_NAME", Objects.toString(dotenv.get("DB_NAME")));
+			System.setProperty("DB_USERNAME", Objects.toString(dotenv.get("DB_USERNAME")));
+			System.setProperty("DB_PASSWORD", Objects.toString(dotenv.get("DB_PASSWORD")));
+			System.setProperty("JWT_SECRET", Objects.toString(dotenv.get("JWT_SECRET")));
+			System.setProperty("ACCESS_TOKEN_EXPIRATION", Objects.toString(dotenv.get("ACCESS_TOKEN_EXPIRATION")));
+			System.setProperty("REFRESH_TOKEN_EXPIRATION", Objects.toString(dotenv.get("REFRESH_TOKEN_EXPIRATION")));
+			System.setProperty("MAIL_USERNAME", Objects.toString(dotenv.get("MAIL_USERNAME")));
+			System.setProperty("MAIL_PASSWORD", Objects.toString(dotenv.get("MAIL_PASSWORD")));
+			System.setProperty("MAIL_FROM", Objects.toString(dotenv.get("MAIL_FROM")));
+			System.setProperty("OTP_EXPIRATION_MINUTES", Objects.toString(dotenv.get("OTP_EXPIRATION_MINUTES")));
+		}
+
+		SpringApplication.run(OutfyBackendApplication.class, args);
+	}
+
+}
