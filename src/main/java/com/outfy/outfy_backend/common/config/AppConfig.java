a0cc2f95a08ccd93188c84b18c0e2624c9b02a76
DangQuangSE <quangndse180246@fpt.edu.vn>
2026-03-17 17:47:20 +0700

    fix cors

--- src/main/java/com/outfy/outfy_backend/common/config/AppConfig.java
+++ src/main/java/com/outfy/outfy_backend/common/config/AppConfig.java
@@ -15,7 +15,7 @@ public class AppConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setExposedHeaders(List.of("Authorization"));
